0860eea7f28fb5c9b8ba01a35d23c3766d3cf24f
Minjae Gwon <beta.underalpha@gmail.com>
2024-01-02 16:40:04 +0900

    feat(assn5): implement hist

--- Assignments/ASSN5/sources/template.cu
+++ Assignments/ASSN5/sources/template.cu
@@ -2,15 +2,23 @@
 
 #define NUM_BINS 4096
 
-#define CUDA_CHECK(ans)                                                   \
+#define CUDA_CHECK(ans)                                                        \
   { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line,
                       bool abort = true) {
   if (code != cudaSuccess) {
-    fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code),
-            file, line);
+    fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file,
+            line);
     if (abort)
       exit(code);
+  }
+}
+
+__global__ void hist(unsigned int *input, unsigned int *bins, int length) {
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (index < length) {
+    atomicAdd(&bins[input[index] / NUM_BINS], 1);
   }
 }
 
@@ -26,7 +34,7 @@ int main(int argc, char *argv[]) {
 
   gpuTKTime_start(Generic, "Importing data and creating memory on host");
   hostInput = (unsigned int *)gpuTKImport(gpuTKArg_getInputFile(args, 0),
-                                       &inputLength, "Integer");
+                                          &inputLength, "Integer");
   hostBins = (unsigned int *)malloc(NUM_BINS * sizeof(unsigned int));
   gpuTKTime_stop(Generic, "Importing data and creating memory on host");
 
@@ -35,11 +43,17 @@ int main(int argc, char *argv[]) {
 
   gpuTKTime_start(GPU, "Allocating GPU memory.");
   //@@ Allocate GPU memory here
+  cudaMalloc(&deviceInput, inputLength * sizeof(unsigned int));
+  cudaMalloc(&deviceBins, NUM_BINS * sizeof(unsigned int));
+
   CUDA_CHECK(cudaDeviceSynchronize());
   gpuTKTime_stop(GPU, "Allocating GPU memory.");
 
   gpuTKTime_start(GPU, "Copying input memory to the GPU.");
   //@@ Copy memory to the GPU here
+  cudaMemcpy(deviceInput, hostInput, inputLength * sizeof(unsigned int),
+             cudaMemcpyHostToDevice);
+
   CUDA_CHECK(cudaDeviceSynchronize());
   gpuTKTime_stop(GPU, "Copying input memory to the GPU.");
 
@@ -48,15 +62,27 @@ int main(int argc, char *argv[]) {
   gpuTKLog(TRACE, "Launching kernel");
   gpuTKTime_start(Compute, "Performing CUDA computation");
   //@@ Perform kernel computation here
+
+  dim3 blockDim(128);
+  dim3 gridDim(ceil((float)inputLength / blockDim.x));
+
+  hist<<<gridDim, blockDim>>>(deviceInput, deviceBins, inputLength);
+
   gpuTKTime_stop(Compute, "Performing CUDA computation");
 
   gpuTKTime_start(Copy, "Copying output memory to the CPU");
   //@@ Copy the GPU memory back to the CPU here
+  cudaMemcpy(hostBins, deviceBins, inputLength * sizeof(unsigned int),
+             cudaMemcpyDeviceToHost);
+
   CUDA_CHECK(cudaDeviceSynchronize());
   gpuTKTime_stop(Copy, "Copying output memory to the CPU");
 
   gpuTKTime_start(GPU, "Freeing GPU Memory");
   //@@ Free the GPU memory here
+  cudaFree(deviceInput);
+  cudaFree(deviceBins);
+
   gpuTKTime_stop(GPU, "Freeing GPU Memory");
 
   // Verify correctness
