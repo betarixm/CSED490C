370ae4c3899f872c9ffca3b1ba4202e8bed88e85
Minjae Gwon <beta.underalpha@gmail.com>
2024-01-02 16:39:54 +0900

    style(assn3): apply clang-format

--- Assignments/ASSN3/sources/template.cu
+++ Assignments/ASSN3/sources/template.cu
@@ -1,6 +1,5 @@
 #include <gputk.h>
 
-
 __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
   int index = threadIdx.x + blockIdx.x * blockDim.x;
   if (index < len) {
@@ -35,34 +34,29 @@ int main(int argc, char **argv) {
 
   //@@ Allocate GPU memory here using pinned memory here
 
-  //@@ Create and setup streams 
-  //@@ Calculate data segment size of input data processed by each stream 
+  //@@ Create and setup streams
+  //@@ Calculate data segment size of input data processed by each stream
 
- 
   gpuTKTime_start(Compute, "Performing CUDA computation");
-  //@@ Perform parallel vector addition with different streams. 
-  for (unsigned int s = 0; s<numStreams; s++){
-          //@@ Asynchronous copy data to the device memory in segments 
-          //@@ Calculate starting and ending indices for per-stream data
+  //@@ Perform parallel vector addition with different streams.
+  for (unsigned int s = 0; s < numStreams; s++) {
+    //@@ Asynchronous copy data to the device memory in segments
+    //@@ Calculate starting and ending indices for per-stream data
 
-          //@@ Invoke CUDA Kernel
-          //@@ Determine grid and thread block sizes (consider ococupancy)     
+    //@@ Invoke CUDA Kernel
+    //@@ Determine grid and thread block sizes (consider ococupancy)
 
-          //@@ Asynchronous copy data from the device memory in segments 
-
+    //@@ Asynchronous copy data from the device memory in segments
   }
 
   //@@ Synchronize
 
   gpuTKTime_stop(Compute, "Performing CUDA computation");
 
-
   gpuTKTime_start(GPU, "Freeing Pinned Memory");
   //@@ Destory cudaStream
 
-
   //@@ Free the GPU memory here
-
 
   gpuTKTime_stop(GPU, "Freeing Pinned Memory");
 
