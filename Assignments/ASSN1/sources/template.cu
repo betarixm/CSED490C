40ecab5955c8fe8f25ce456f4dd1d4d247988046
Minjae Gwon <beta.underalpha@gmail.com>
2024-01-02 16:38:45 +0900

    feat(assn1): implement vector addition

--- Assignments/ASSN1/sources/template.cu
+++ Assignments/ASSN1/sources/template.cu
@@ -2,6 +2,11 @@
 
 __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
   //@@ Insert code to implement vector addition here
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (index < len) {
+    out[index] = in1[index] + in2[index];
+  }
 }
 
 int main(int argc, char **argv) {
@@ -28,29 +33,45 @@ int main(int argc, char **argv) {
 
   gpuTKTime_start(GPU, "Allocating GPU memory.");
   //@@ Allocate GPU memory here
+  cudaMalloc((void **)&deviceInput1, inputLength * sizeof(float));
+  cudaMalloc((void **)&deviceInput2, inputLength * sizeof(float));
+  cudaMalloc((void **)&deviceOutput, inputLength * sizeof(float));
 
   gpuTKTime_stop(GPU, "Allocating GPU memory.");
 
   gpuTKTime_start(GPU, "Copying input memory to the GPU.");
   //@@ Copy memory to the GPU here
+  cudaMemcpy(deviceInput1, hostInput1, inputLength * sizeof(float),
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceInput2, hostInput2, inputLength * sizeof(float),
+             cudaMemcpyHostToDevice);
 
   gpuTKTime_stop(GPU, "Copying input memory to the GPU.");
 
   //@@ Initialize the grid and block dimensions here
+  dim3 blockDim(256);
+  dim3 gridDim(ceil((float)inputLength / (float)blockDim.x));
 
   gpuTKTime_start(Compute, "Performing CUDA computation");
   //@@ Launch the GPU Kernel here
+  vecAdd<<<gridDim, blockDim>>>(deviceInput1, deviceInput2, deviceOutput,
+                                inputLength);
 
   cudaDeviceSynchronize();
   gpuTKTime_stop(Compute, "Performing CUDA computation");
 
   gpuTKTime_start(Copy, "Copying output memory to the CPU");
   //@@ Copy the GPU memory back to the CPU here
+  cudaMemcpy(hostOutput, deviceOutput, inputLength * sizeof(float),
+             cudaMemcpyDeviceToHost);
 
   gpuTKTime_stop(Copy, "Copying output memory to the CPU");
 
   gpuTKTime_start(GPU, "Freeing GPU Memory");
   //@@ Free the GPU memory here
+  cudaFree(deviceInput1);
+  cudaFree(deviceInput2);
+  cudaFree(deviceOutput);
 
   gpuTKTime_stop(GPU, "Freeing GPU Memory");
 
