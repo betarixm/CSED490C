5b47ef75ea37f28827dfcfb023cbae526ebdcb1c
Minjae Gwon <beta.underalpha@gmail.com>
2024-01-02 16:39:54 +0900

    feat(assn3): implement streamed vector addition

--- Assignments/ASSN3/sources/template.cu
+++ Assignments/ASSN3/sources/template.cu
@@ -1,5 +1,7 @@
 #include <gputk.h>
 
+#define NUM_STREAMS 16
+
 __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
   int index = threadIdx.x + blockIdx.x * blockDim.x;
   if (index < len) {
@@ -33,30 +35,85 @@ int main(int argc, char **argv) {
   gpuTKTime_start(GPU, "Allocating Pinned memory.");
 
   //@@ Allocate GPU memory here using pinned memory here
+  cudaHostRegister(hostInput1, inputLength * sizeof(float),
+                   cudaHostRegisterMapped);
+  cudaHostRegister(hostInput2, inputLength * sizeof(float),
+                   cudaHostRegisterMapped);
+  cudaHostRegister(hostOutput, inputLength * sizeof(float),
+                   cudaHostRegisterMapped);
+
+  cudaMalloc(&deviceInput1, inputLength * sizeof(float));
+  cudaMalloc(&deviceInput2, inputLength * sizeof(float));
+  cudaMalloc(&deviceOutput, inputLength * sizeof(float));
 
   //@@ Create and setup streams
+  numStreams = NUM_STREAMS;
+
+  cudaStream_t *streams =
+      (cudaStream_t *)malloc(numStreams * sizeof(cudaStream_t));
+
+  for (unsigned int s = 0; s < numStreams; s++) {
+    cudaStreamCreate(&(streams[s]));
+  }
+
   //@@ Calculate data segment size of input data processed by each stream
+  int segmentLength = ceil((float)inputLength / numStreams);
 
   gpuTKTime_start(Compute, "Performing CUDA computation");
   //@@ Perform parallel vector addition with different streams.
   for (unsigned int s = 0; s < numStreams; s++) {
     //@@ Asynchronous copy data to the device memory in segments
+    int streamSegmentLength = std::max(
+        0, std::min(inputLength - (int)s * segmentLength, segmentLength));
+
+    cudaMemcpyAsync(deviceInput1 + s * segmentLength,
+                    hostInput1 + s * segmentLength,
+                    streamSegmentLength * sizeof(float), cudaMemcpyHostToDevice,
+                    streams[s]);
+
+    cudaMemcpyAsync(deviceInput2 + s * segmentLength,
+                    hostInput2 + s * segmentLength,
+                    streamSegmentLength * sizeof(float), cudaMemcpyHostToDevice,
+                    streams[s]);
+
     //@@ Calculate starting and ending indices for per-stream data
 
     //@@ Invoke CUDA Kernel
     //@@ Determine grid and thread block sizes (consider ococupancy)
+    dim3 blockDim(256);
+    dim3 gridDim(ceil((float)streamSegmentLength / blockDim.x));
+
+    vecAdd<<<gridDim, blockDim, 0, streams[s]>>>(
+        deviceInput1 + s * segmentLength, deviceInput2 + s * segmentLength,
+        deviceOutput + s * segmentLength, segmentLength);
 
     //@@ Asynchronous copy data from the device memory in segments
+    cudaMemcpyAsync(hostOutput + s * segmentLength,
+                    deviceOutput + s * segmentLength,
+                    streamSegmentLength * sizeof(float), cudaMemcpyDeviceToHost,
+                    streams[s]);
   }
 
   //@@ Synchronize
-
+  for (unsigned int s = 0; s < numStreams; s++) {
+    cudaStreamSynchronize(streams[s]);
+  }
   gpuTKTime_stop(Compute, "Performing CUDA computation");
 
   gpuTKTime_start(GPU, "Freeing Pinned Memory");
   //@@ Destory cudaStream
+  for (unsigned int s = 0; s < numStreams; s++) {
+    cudaStreamDestroy(streams[s]);
+  }
 
   //@@ Free the GPU memory here
+  cudaFree(deviceInput1);
+  cudaFree(deviceInput2);
+  cudaFree(deviceOutput);
+
+  cudaHostUnregister(hostInput1);
+  cudaHostUnregister(hostInput2);
+  cudaHostUnregister(hostOutput);
 
   gpuTKTime_stop(GPU, "Freeing Pinned Memory");
 
